ae1ba9b441e8799fe8edd26d5967fb4d8a5994a2
moulz04 <amoulaye002@gmail.com>
2026-05-02 22:14:28 +0000

    connection a la base de donnee

---
+++ MetierMemoire/Model/BdMemoireContext.cs
@@ -0,0 +1,23 @@
+﻿
+using MySql.Data.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace MetierMemoire.Model
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class BdMemoireContext:DbContext
+    {
+
+
+        public BdMemoireContext() : base("name=connMemoire")
+        {
+        }
+
+        public DbSet<Memoire> Memoires { get; set; }
+    }
+}
---
+++ MetierMemoire/Model/Memoire.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MetierMemoire.Model
+{
+    public class Memoire
+    {
+        [Key]
+        public int IDMemoire { get; set; }
+
+
+        [Required, MaxLength(2000)]
+        public string SujetMemoire { get; set; }
+
+
+        [Required, MaxLength(100000)]
+        public string DescriptionMemoire { get; set; }
+
+
+        [Required]
+        public int AnneeMemoire { get; set; }
+
+
+
+    }
+}
