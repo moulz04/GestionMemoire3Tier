927d810b118d4f789af513110c88782bbdd09f13
moulz04 <amoulaye002@gmail.com>
2026-05-02 22:45:49 +0000

    ajout des methode -liste des memoire -Memoire via son id -AddMemoire dans serviceMemoire

---
+++ MetierMemoire/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace MetierMemoire.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MetierMemoire.Model.BdMemoireContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = false;
+        }
+
+        protected override void Seed(MetierMemoire.Model.BdMemoireContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ MetierMemoire/Service/MemoireService.cs
@@ -0,0 +1,56 @@
+﻿using MetierMemoire.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MetierMemoire.Service
+{
+    public class MemoireService
+    {
+        BdMemoireContext db = new BdMemoireContext();
+
+        /// <summary>
+        /// Cette methode renvoie la liste de tous les memoires disponibles dans la base de données
+        /// </summary>
+        /// <returns></returns>
+
+        public List<Memoire> getAllMemoire()
+        {
+            return db.Memoires.ToList();
+        }
+
+        /// <summary>
+        /// Renvoie le memoire correspondant à l'identifiant passé en paramètre
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+
+        public Memoire GetMemoire(int? id)
+        {
+            return db.Memoires.Find(id);
+        }
+
+
+        /// <summary>
+        /// Permet l'engeristrement d'un memoire dans la base de données
+        /// </summary>
+        /// <param name="memo"></param>
+        /// <returns></returns>
+
+        public bool AddMemoire(Memoire memo)
+        {
+            try
+            {
+                db.Memoires.Add(memo);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //todo implementer la gestion des erreurs
+            }
+            return false;
+        }
+    }
+}
